1f1fe2a6624364c429ae3980d5aecafbda5ce7cd
jmonto55 <jmonto55@gmail.com>
2023-02-03 16:06:10 -0500

    Add modal functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './style.css';
 import './modules/tools.js';
+
+const close = document.querySelector('.close');
+const modal = document.querySelector('.modal');
+const toggleModal = () => {
+  modal.classList.toggle('invisible');
+};
+close.onclick = () => toggleModal();
